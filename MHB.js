7496076ecf9fbaaa1919a1094adbf0b67c672eaf
dlrowdimfoyo <tomomilandsman@gmail.com>
2019-08-14 17:38:59 -0400

    Each tile can only be clicked on four times

--- MHB.js
+++ MHB.js
@@ -1,15 +1,54 @@
 var tiles = document.getElementsByClassName("tile");
+var man = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var sou = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var pin = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var wind = [0, 0, 0, 0]
+var dragon = [0, 0, 0]
 
-for (var i = 0; i < tiles.length; i++) {
-    tiles[i].addEventListener('click', function(e){tile_select(e.target)});
-}
-
-document.getElementsByClassName("tile").addEventListener("click", function(e){tile_select(e.target)})
-
-function tile_select(img) {
+var tile_select = function(e) {
   var new_tile = document.createElement("img")
+  var img = e.target
   var src = img.src
+  var id = img.id
+  var suit = id.substr(0, 3)
+  var numb = Number(id.substr(3))
   new_tile.src = src
   new_tile.id = "tile"
   document.getElementById("selected_tiles").appendChild(new_tile)
+  if (suit == "man") {
+	  man[numb - 1]++
+	  if (man[numb - 1] == 4) {
+		  disable_tile(id)
+	  }
+  } else if (suit == "sou") {
+	  sou[numb - 1]++
+	  if (sou[numb - 1] == 4) {
+		  disable_tile(id)
+	  }
+  } else if (suit == "pin") {
+	  pin[numb - 1]++
+	  if (pin[numb - 1] == 4) {
+		  disable_tile(id)
+	  }
+  } else if (suit == "win") {
+	  wind[numb - 1]++
+	  if (wind[numb - 1] == 4) {
+		  disable_tile(id)
+	  }
+  } else {
+	  dragon[numb - 1]++
+	  if (dragon[numb - 1] == 4) {
+		  disable_tile(id)
+	  }
+  }
+}
+
+var disable_tile = function(id) {
+	document.getElementById(id).style.opacity = "0.2"
+	document.getElementById(id).style.cursor = "default"
+	document.getElementById(id).removeEventListener("click", tile_select)
+}
+
+for (var i = 0; i < tiles.length; i++) {
+    tiles[i].addEventListener('click', tile_select);
 }
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ My attempt to create a mahjong hand builder to easily select tiles to display a 
 
 Aug. 13: I now have everything centered with a fixed width. There is a box for where the selected tiles show up and a box for the final hand, although that currently contains nothing.
 
-Next steps: limit to four of each tile, open melds, kans, limit hand, reorganize hand, drawn tile
+Aug. 14: I've limited the number of times each tile can be clicked to 4.
+
+Next steps: open melds, kans, limit hand, reorganize hand, drawn tile
